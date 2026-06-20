b524c5a4b43266704dcb2120e4967dec80603b51
reggy williams <reggywilliams@gmail.com>
2026-06-21 00:03:29 +0100

    Updating docu.js images path to include BasePath url

--- src/data/docu.js
+++ src/data/docu.js
@@ -6,8 +6,8 @@ export const documentaries = [
     year: 2026,
     maturity: "13+",
     duration: "1h 48m",
-    image: "/images/kidnapping-economy.png",
-    backdrop: "/images/kidnapping-economy.png",
+    image: `${import.meta.env.BASE_URL}images/kidnapping-economy.png`,
+    backdrop: `${import.meta.env.BASE_URL}images/kidnapping-economy.png`,
     description: "An investigative review of crime, terrorism and the questions they raise"
   },
   {
@@ -17,8 +17,8 @@ export const documentaries = [
     year: 2025,
     maturity: "16+",
     duration: "8 episodes",
-    image: "/images/commerce-in-lagos.png",
-    backdrop: "/images/commerce-in-lagos.png",
+    image: `${import.meta.env.BASE_URL}images/commerce-in-lagos.png`,
+    backdrop: `${import.meta.env.BASE_URL}images/commerce-in-lagos.png`,
     description: "Lagos the land of opportunities."
   },
   {
@@ -28,8 +28,8 @@ export const documentaries = [
     year: 2026,
     maturity: "16+",
     duration: "8 episodes",
-    image: "/images/ja-pada.png",
-    backdrop: "/images/ja-pada-wide.png",
+    image: `${import.meta.env.BASE_URL}images/ja-pada.png`,
+    backdrop: `${import.meta.env.BASE_URL}images/ja-pada-wide.png`,
     description: "Experiences of diasporans that return to Nigeria to face the tough terrain on ground."
   },
   {
@@ -39,8 +39,8 @@ export const documentaries = [
     year: 2026,
     maturity: "13+",
     duration: "1h 48m",
-    image: "/images/nigerian-politics.png",
-    backdrop: "/images/nigerian-politics-wide.png",
+    image: `${import.meta.env.BASE_URL}images/nigerian-politics.png`,
+    backdrop: `${import.meta.env.BASE_URL}images/nigerian-politics-wide.png`,
     description: "The twists and turns of Nigerian politics as elections draw near."
   }
 
